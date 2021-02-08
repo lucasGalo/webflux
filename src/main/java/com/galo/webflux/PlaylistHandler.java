ba4bcc1ef77e8160465f4f73668565a937726d8c
lucas_galo <lucas_d>
2021-02-08 18:21:11 -0300

    Classe: Router, Handler

---
+++ src/main/java/com/galo/webflux/PlaylistHandler.java
@@ -0,0 +1,40 @@
+package com.galo.webflux;
+
+import com.galo.webflux.api.document.Playlist;
+import com.galo.webflux.api.services.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
+import reactor.core.publisher.Mono;
+
+@Component
+public class PlaylistHandler {
+
+    @Autowired
+    PlaylistService service;
+
+    public Mono<ServerResponse> findAll(ServerRequest request) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.finAll(), Playlist.class);
+    }
+
+    public Mono<ServerResponse> findById(ServerRequest request) {
+        String id = request.pathVariable("id");
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.findById(id), Playlist.class);
+    }
+
+    public Mono<ServerResponse> save(ServerRequest request) {
+        final Mono<Playlist> playlist = request.bodyToMono(Playlist.class);
+
+        return ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fromPublisher(playlist.flatMap(service::save), Playlist.class));
+    }
+}
